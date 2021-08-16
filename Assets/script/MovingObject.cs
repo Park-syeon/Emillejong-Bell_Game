057a82f24ee776f55b03322db58d35bc2e647cf4
Seo-yeon Park <scp08081@naver.com>
2021-08-16 20:01:27 +0900

    캐릭터 그래픽,애니메이션 추가/ui를 위한 manager 일단 삭제

--- Assets/script/MovingObject.cs
+++ Assets/script/MovingObject.cs
@@ -7,16 +7,21 @@ public class MovingObject : MonoBehaviour
     static public MovingObject instance;
     public string currentMapName; //transferMap 스크립트에 있는 transferMapName 변수의 값을 지정
     public float speed;
-    public GameManager manager;
-    Rigidbody2D rigid;
-    float h, v;
-    Vector3 dirVec;//현재 바라보고 있는 방향 값을 가진 변수
     private Vector3 vector;
-    GameObject scanObject;
+    //shift 키를 누르면 빨리 달릴 수 있도록 함.
+    public float runSpeed;
+    private float applyRunSpeed;
+    private bool applyRunFlag = false;
+    //tile 단위로 캐릭터가 움직이도록 함.
+    public int walkCount;
+    private int currentWalkCount;
 
+    private bool canMove = true;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         if (instance == null)
         {
             DontDestroyOnLoad(this.gameObject);
@@ -27,58 +32,62 @@ public class MovingObject : MonoBehaviour
             Destroy(this.gameObject);
         }      
     }
+    IEnumerator MoveCoroutine()
+    {
+        while(Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                applyRunSpeed = runSpeed;
+                applyRunFlag = true;
+            }
+            else
+            {
+                applyRunSpeed = 0;
+                applyRunFlag = false;
+            }
+        vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+            if (vector.x != 0)
+                vector.y = 0;
+
+        animator.SetFloat("DirX", vector.x);
+        animator.SetFloat("DirY", vector.y);
+        animator.SetBool("Walking", true);
+
+        while (currentWalkCount < walkCount)
+        {
+            if (vector.x != 0)
+            {
+                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+            }
+            else if (vector.y != 0)
+            {
+                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+            }
+            if (applyRunFlag)
+                currentWalkCount++;
+            currentWalkCount++;
+            yield return new WaitForSeconds(0.01f);
+        }
+        currentWalkCount = 0;
+        }
+        animator.SetBool("Walking", false);
+        canMove = true;        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        //Move Value
-        h = manager.isAction ? 0 : Input.GetAxisRaw("Horizontal");
-        v = manager.isAction ? 0 : Input.GetAxisRaw("Vertical");
-        //Check Button Down & Up
-        bool hDown = manager.isAction ? false : Input.GetButtonDown("Horizontal");
-        bool vDown = manager.isAction ? false : Input.GetButtonDown("Vertical");
-        bool hUp = manager.isAction ? false : Input.GetButtonUp("Horizontal");
-        bool vUp = manager.isAction ? false : Input.GetButtonUp("Vertical");
-
         //Move
-        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        if (canMove)
         {
-            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
-
-            if(vector.x != 0)
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
             {
-                transform.Translate(vector.x * speed, 0, 0);
-            }
-            else if(vector.y != 0)
-            {
-                transform.Translate(0, vector.y * speed, 0);
+                canMove = false;
+                StartCoroutine(MoveCoroutine());
             }
         }
 
-        //Direction
-        if (vDown && v == 1)
-            dirVec = Vector3.up;
-        else if (vDown && v == -1)
-            dirVec = Vector3.down;
-        else if (hDown && h == -1)
-            dirVec = Vector3.left;
-        else if (hDown && h == 1)
-            dirVec = Vector3.right;
-        if (Input.GetButtonDown("Jump") && scanObject != null)
-            manager.Action(scanObject);
-
-    }
-    void FixedUpdate()
-    {
-        //Ray
-        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0,1,0));
-        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
-
-        if (rayHit.collider != null)
-        {
-            scanObject = rayHit.collider.gameObject;
-        }
-        else
-            scanObject = null;
     }
 }
