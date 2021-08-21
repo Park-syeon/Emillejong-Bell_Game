a795283cc88402af49dc40f1c05220eada273f25
happysujin <50164158+happysujin@users.noreply.github.com>
2021-08-21 21:17:40 +0900

    ordermanager에서 bookshelf를 못찾아서 스님방에 bookshelf 있는 버전의 ordermanager, 템플씬에는 없는 ordermanager 스크립트 넣음. usemonkdiary34에 싱클톤 추가, generalitem 인벤토리 끄는거 수정

--- Assets/Scripts/GameManagerScripts/UseMonkDiary4.cs
+++ Assets/Scripts/GameManagerScripts/UseMonkDiary4.cs
@@ -7,6 +7,22 @@ public class UseMonkDiary4 : MonoBehaviour
 {
     public static UseMonkDiary4 instance;
 
+    #region ΩÃ±€≈Ê
+    private void Awake()    //ΩÃ±€≈Ê!!
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+    }
+    #endregion
+
     private bool activated;
 
     public GameObject diary4;
