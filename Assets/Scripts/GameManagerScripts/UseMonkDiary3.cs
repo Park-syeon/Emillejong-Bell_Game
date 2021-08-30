3ffc5878f6d6e374b689663863eb2d1bfd0d4824
happysujin <50164158+happysujin@users.noreply.github.com>
2021-08-30 22:30:26 +0900

    하는중

--- Assets/Scripts/GameManagerScripts/UseMonkDiary3.cs
+++ Assets/Scripts/GameManagerScripts/UseMonkDiary3.cs
@@ -66,7 +66,10 @@ public class UseMonkDiary3 : MonoBehaviour
 //            Debug.Log("여기까지2");
             Inventory.instance.GetAnItem(Constants.real_monkdiary3_ID);
             SeveralDialogue.instance.AfterGetDiary3();
-            Inventory.instance.GetAnItem(Constants.previous_monkdiary4_ID);
+            if (FindObjectOfType<setActiveDiary4>())
+            {
+                setActiveDiary4.instance.active();
+            }
  //           Debug.Log("여기까지3");
             Inventory.instance.RemoveAnItem(Constants.previous_monkdiary3_ID); ;
 //            Debug.Log("여기까지4");
